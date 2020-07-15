7e7773936bd40216ef043d28e5bd1bc571026e2e
vishnu teja paturu <vishnu.paturu@mindtree.com>
2020-07-16 01:02:31 +0530

    Updates

---
+++ precache-manifest.e0bc93d93e367d5c34c2455e1849a7cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c70bbcf4aef14a9a3379f4b2350876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "307215ef69eff75d7b12",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43e5f69650c8746437db",
+    "url": "/static/js/2.9afbd95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9afbd95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307215ef69eff75d7b12",
+    "url": "/static/js/main.3281a819.chunk.js"
+  },
+  {
+    "revision": "5ae8de4bfe03964be277",
+    "url": "/static/js/runtime-main.8cc3ca45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
