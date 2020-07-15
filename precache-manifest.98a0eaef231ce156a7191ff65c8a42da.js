c927f7a022faa8f46807be4bc182215a4601aee9
vishnu teja paturu <vishnu.paturu@mindtree.com>
2020-07-16 01:17:18 +0530

    Updates

---
+++ precache-manifest.98a0eaef231ce156a7191ff65c8a42da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09510573f0be2cabbb04f651728d6234",
+    "url": "/vishnu-teja.github.io/index.html"
+  },
+  {
+    "revision": "eeb65602a513099a5f28",
+    "url": "/vishnu-teja.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43e5f69650c8746437db",
+    "url": "/vishnu-teja.github.io/static/js/2.9afbd95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vishnu-teja.github.io/static/js/2.9afbd95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb65602a513099a5f28",
+    "url": "/vishnu-teja.github.io/static/js/main.ebbed161.chunk.js"
+  },
+  {
+    "revision": "5baf3cc7714b87802ba1",
+    "url": "/vishnu-teja.github.io/static/js/runtime-main.b53fa567.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vishnu-teja.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
