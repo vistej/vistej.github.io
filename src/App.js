2e2bcea5760f0eb256def7335b657c2e26181e8e
vishnu <vteja@tropicalsmoothie.com>
2021-07-07 23:09:46 +0530

    updated gif

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Site under construction</h3>
-        <img src={'https://media.giphy.com/media/eK74RBKoSO2K4/source.gif'} alt="House drinking coffee" />
+        <img src={'https://media.giphy.com/media/145s3kBsTZGliM/source.gif'} alt="House drinking coffee" />
       </header>
     </div>
   );
