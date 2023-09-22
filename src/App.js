90de8ac5c2c0c344242a7441712bc8a0159b792c
vistej <teja.pvt@gmail.com>
2023-09-22 00:47:12 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h3>Site under construction</h3>
         <img src={'https://media.giphy.com/media/145s3kBsTZGliM/source.gif'} alt="House drinking coffee" />
+        <iframe title='strava' height='160' width='300' frameborder='0' allowtransparency='true' scrolling='no' src=''></iframe>
       </header>
     </div>
   );
